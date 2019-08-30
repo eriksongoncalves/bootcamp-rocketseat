89b0094e41394f4cbb974ab53dd1c6c11d08b66b
Erikson Bezerra <eriksonbezerra@MacBook-Air-de-Erikson.local>
2019-08-30 20:17:57 -0300

    Configurando MongoDB e Notificando novos agendamentos

--- gobarber/backend/src/app/controllers/AppointmentController.js
+++ gobarber/backend/src/app/controllers/AppointmentController.js
@@ -1,7 +1,11 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
     async index(req, res) {
@@ -55,11 +59,42 @@ class AppointmentController {
             return res.status(401).json({ error: 'You can only create appointments with providers' });
         }
 
+        const hourStart = startOfHour(parseISO(date));
+
+        if (isBefore(hourStart, new Date())) {
+            return res.status(400).json({ error: "Past dates are not permitted" })
+        }
+
+        const checkAvailability = await Appointment.findOne({
+            where: {
+                provider_id,
+                canceled_at: null,
+                date: hourStart
+            }
+        });
+
+        if (checkAvailability) {
+            return res.status(400).json({ error: "Appointment date is not avaiable" });
+        }
+
         const appointment = await Appointment.create({
             user_id: req.userId,
             provider_id,
             date
         });
+
+        const user = await User.findByPk(req.userId);
+        const formattedDate = format(
+            hourStart,
+            "'dia ' dd 'de ' MMMM', às' H:MM'h'",
+            { locale: pt }
+        )
+
+        await Notification.create({
+            content: `Novo agendamento de ${user.name} para ${formattedDate}`,
+            user: provider_id,
+
+        })
 
         return res.json(appointment);
     }
